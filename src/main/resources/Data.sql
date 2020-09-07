4bc264d2a45a9f816186d333f54e84d6eb764252
Mugdhaa Patankar <mpatanka@uci.edu>
2020-09-07 02:01:49 -0700

    Added new categories

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -3,7 +3,7 @@ insert into user values (2, 'John', 'john@gmail.com')
 insert into user values (3, 'Daniella', 'daniella@gmail.com')
 
 insert into category values(1, 'Travel')
-insert into category values(2, 'Auto Loan')
-insert into category values(3, 'Student Loan')
+insert into category values(2, 'Groceries')
+insert into category values(3, 'Entertainment')
 insert into category values(4, 'Business')
 
